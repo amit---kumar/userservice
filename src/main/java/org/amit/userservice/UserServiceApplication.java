1627d7023a544e8cfae6a7ccd14292cf516968a0
abishwas@manh.com <abishwas@manh.com>
2024-10-02 14:49:01 +0530

    embedded tomcat

--- src/main/java/org/amit/userservice/UserServiceApplication.java
+++ src/main/java/org/amit/userservice/UserServiceApplication.java
@@ -2,10 +2,9 @@ package org.amit.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UserServiceApplication extends SpringBootServletInitializer {
+public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
